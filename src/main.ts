6209c30913102ba7124369918faf46a3a6c45e22
Niloufar <153923465+Niloufar88@users.noreply.github.com>
2026-08-16 18:15:25 +0330

    init scss partials

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-//@ts-ignore
-import "./styles/style.scss";
+// @ts-ignore
+import "./styles/main.scss";
 const title = document.getElementById("main-title") as HTMLHeadingElement;
 
 title.innerText = `was man hier sieht, ist ein Test.`;
