0c56513b640a567e4730eb1b53e583f546123d70
Niloufar <153923465+Niloufar88@users.noreply.github.com>
2026-08-21 22:40:33 +0330

    adding hover effect on radio btns + start btn

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ import "./styles/main.scss";
 import { cards } from "./type";
 import { renderBoardElements, startGame } from "./game";
 
-const startBtn = document.getElementById("startBtn") as HTMLButtonElement;
-const startBtnArrow = document.getElementById(
-  "startBtn-arrow",
+const playBtn = document.getElementById("playBtn") as HTMLButtonElement;
+const playBtnArrow = document.getElementById(
+  "playBtn-arrow",
 ) as HTMLImageElement;
 const arrowNormal = "/assets/icons/landing-page/play-arrow.svg";
 const arrowHover = "/assets/icons/landing-page/play-arrow-hover.svg";
@@ -16,17 +16,47 @@ const exitBtnIcon = exitBtn.querySelector("img") as HTMLImageElement;
 const exitNormal = "/assets/icons/food/exit-default.svg";
 const exitHover = "/assets/icons/food/exit-hover.svg";
 
-startBtn.addEventListener("mouseover", () => {
-  startBtnArrow.src = arrowHover;
+const radioButtonsContainer = document.querySelectorAll(
+  ".options-container",
+) as NodeListOf<HTMLDivElement>;
+
+radioButtonsContainer.forEach((container) => {
+  container.addEventListener("mouseenter", () => {
+    const radioButton = container.querySelector(
+      'input[type="radio"]',
+    ) as HTMLInputElement;
+    const labelText = container.querySelector("label") as HTMLLabelElement;
+    if (radioButton) {
+      radioButton.checked = true;
+      labelText.style.fontWeight = "bold";
+    }
+  });
 });
 
-startBtn.addEventListener("mouseleave", () => {
-  startBtnArrow.src = arrowNormal;
+radioButtonsContainer.forEach((container) => {
+  container.addEventListener("mouseleave", () => {
+    const radioButton = container.querySelector(
+      'input[type="radio"]',
+    ) as HTMLInputElement;
+    const labelText = container.querySelector("label") as HTMLLabelElement;
+    if (radioButton) {
+      radioButton.checked = false;
+      labelText.style.fontWeight = "normal";
+    }
+  });
 });
 
-startBtn.addEventListener("click", () => {
-  renderBoardElements();
+playBtn.addEventListener("mouseover", () => {
+  playBtnArrow.src = arrowHover;
 });
+
+playBtn.addEventListener("mouseleave", () => {
+  playBtnArrow.src = arrowNormal;
+});
+
+// playBtn.addEventListener("click", () => {
+//   renderBoardElements();
+// });
 
 exitBtn?.addEventListener("mouseover", () => {
   if (exitBtnIcon) exitBtnIcon.src = exitHover;
