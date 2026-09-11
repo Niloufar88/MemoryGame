c1bae15bdacae5b61b1225d1ac0fdad4d710361b
Niloufar <153923465+Niloufar88@users.noreply.github.com>
2026-09-11 16:14:14 +0330

    addign TSDoc

--- src/main.ts
+++ src/main.ts
@@ -14,72 +14,141 @@ import {
 
 import { openDialog } from "./screens-dialog-reset";
 
+/**
+ * play button element
+ */
 const PLAY_BTN = document.getElementById("playBtn") as HTMLButtonElement;
+
+/**
+ * play button arrow container
+ */
 const PLAY_BUTTON_ARROW = document.getElementById(
   "playBtn-arrow",
 ) as HTMLImageElement;
+
+/**
+ * play button svg in default mode
+ */
 const ARROW_NORMAL = "/assets/icons/landing-page/play-arrow.svg";
+
+/**
+ * play button arrow in hover mode
+ */
 const ARROW_HOVER = "/assets/icons/landing-page/play-arrow-hover.svg";
 
+/**
+ * start button element
+ */
 const START_BTN = document.getElementById("start-Btn") as HTMLButtonElement;
 
+/**
+ * exit button element
+ */
 const EXIT_BTN = document.querySelector(".exit-button") as HTMLButtonElement;
+
+/**
+ * exit button icon container
+ */
 const EXIT_BTN_ICON = EXIT_BTN.querySelector("img") as HTMLImageElement;
+
+/**
+ * exit button svg in default mode by food theme
+ */
 const EXIT_NORMAL_ORANGE = "/assets/icons/food/exit-default.svg";
+
+/**
+ * exit button svg in default mode by DA Projects theme
+ */
 const EXIT_NORMAL_BLUE = "/assets/icons/DA/exit-blue-default.svg";
+
+/**
+ * exit button svg in hover mode by both themes
+ */
 const EXIT_HOVER = "/assets/icons/food/exit-hover.svg";
 
+/**
+ * choose player input container
+ */
 const CHOOSE_PLAYER_DIV = document.querySelector(
   ".details__player--options",
 ) as HTMLDivElement;
 
+/**
+ * board size input container
+ */
 const SET_BORAD_SIZE_DIV = document.querySelector(
   ".details__size--options",
 ) as HTMLDivElement;
 
+/**
+ * game theme input container
+ */
 const CHOOSE_GAME_THEME_DIV = document.querySelector(
   ".details__themes--options",
 ) as HTMLDivElement;
 
+/**
+ * game theme preview img container
+ */
 const THEME_PREVIEW_IMG = document.querySelector(
   ".theme-img img",
 ) as HTMLImageElement;
 
+/**
+ * game theme images
+ */
 const THEME_IMAGES: Record<string, string> = {
   DAProjects: "/assets/icons/settings/DA-Projects-Theme Visual .svg",
   foods: "/assets/icons/settings/Food-Theme Visual.svg",
 };
 
+/**
+ * selected settings container
+ */
 export const SETTING_BOX = document.getElementById(
   "setting-box",
 ) as HTMLDivElement;
 
+/**
+ * an object which saves actively selected options in setting page
+ */
 export const ACTIVELY_SELECTED_RADIOS = {
   player: null as HTMLInputElement | null,
   boardSize: null as HTMLInputElement | null,
   gameTheme: null as HTMLInputElement | null,
 };
 
+/**
+ * resetting visuals by selected conatiner to default phase
+ * @param container one of 3 conatiners which conatins radio inputs
+ */
 export function resetContainerVisuals(container: HTMLDivElement): void {
   const RADIO_BTN = container.querySelector(
     'input[type="radio"]',
   ) as HTMLInputElement;
   const LABEL = container.querySelector("label") as HTMLLabelElement;
   const IMG = container.querySelector("img") as HTMLImageElement;
-
   if (RADIO_BTN) RADIO_BTN.checked = false;
   if (LABEL) LABEL.style.fontWeight = "normal";
   if (IMG) IMG.style.visibility = "hidden";
 }
 
+/**
+ * making changes to visual of each input sets
+ * @param container one of 3 conatiners which conatins radio inputs
+ */
 function activateVisualls(container: HTMLDivElement): void {
   const LABEL = container.querySelector("label") as HTMLLabelElement;
   const IMG = container.querySelector("img") as HTMLImageElement;
-
   if (LABEL) LABEL.style.fontWeight = "bold";
   if (IMG) IMG.style.visibility = "visible";
 }
 
+/**
+ * making a loop on all containers to make necessary chenages to their visuals and checked attribute
+ * @param target input target
+ * @param containers html div container which contains input radios
+ */
 function handleRadioBtnSelection(
   target: HTMLInputElement,
   containers: NodeListOf<HTMLDivElement>,
@@ -98,6 +167,9 @@ function handleRadioBtnSelection(
   }
 }
 
+/**
+ * running a click listener on player conatiner and get a value of target to save it for board and make necessary visual changes
+ */
 CHOOSE_PLAYER_DIV.addEventListener("click", (event) => {
   const TARGET = event.target as HTMLInputElement;
   if (TARGET && TARGET.type === "radio") {
@@ -108,6 +180,9 @@ CHOOSE_PLAYER_DIV.addEventListener("click", (event) => {
   }
 });
 
+/**
+ * running a click listener on board size conatiner and get a value of target to save it for board and make necessary visual changes
+ */
 SET_BORAD_SIZE_DIV.addEventListener("click", (event) => {
   const TARGET = event.target as HTMLInputElement;
   if (TARGET && TARGET.type === "radio") {
@@ -118,6 +193,9 @@ SET_BORAD_SIZE_DIV.addEventListener("click", (event) => {
   }
 });
 
+/**
+ * running a click listener on game theme conatiner and get a value of target to save it for board and make necessary visual changes
+ */
 CHOOSE_GAME_THEME_DIV.addEventListener("click", (event) => {
   const TARGET = event.target as HTMLInputElement;
   if (TARGET && TARGET.type === "radio") {
@@ -130,16 +208,30 @@ CHOOSE_GAME_THEME_DIV.addEventListener("click", (event) => {
   }
 });
 
+/**
+ * all input buttons in player container
+ */
 export const PLAYER_BUTTONS = CHOOSE_PLAYER_DIV.querySelectorAll(
   ".options-container",
 ) as NodeListOf<HTMLDivElement>;
+
+/**
+ * all input buttons in game theme container
+ */
 export const THEMES_BUTTONS = CHOOSE_GAME_THEME_DIV.querySelectorAll(
   ".options-container",
 ) as NodeListOf<HTMLDivElement>;
+
+/**
+ * all input buttons in board size container
+ */
 export const BOARD_SIZE_BUTTONS = SET_BORAD_SIZE_DIV.querySelectorAll(
   ".options-container",
 ) as NodeListOf<HTMLDivElement>;
 
+/**
+ * handeling 2 event listeners on a set of input buttons in player container
+ */
 PLAYER_BUTTONS.forEach((container) => {
   container.addEventListener("mouseenter", () => {
     handleMouseEnter(container, ACTIVELY_SELECTED_RADIOS.player);
@@ -149,6 +241,9 @@ PLAYER_BUTTONS.forEach((container) => {
   });
 });
 
+/**
+ * handeling 2 event listeners on a set of input buttons in game theme container
+ */
 THEMES_BUTTONS.forEach((container) => {
   container.addEventListener("mouseenter", () => {
     handleMouseEnter(container, ACTIVELY_SELECTED_RADIOS.gameTheme);
@@ -158,6 +253,9 @@ THEMES_BUTTONS.forEach((container) => {
   });
 });
 
+/**
+ * handeling 2 event listeners on a set of input buttons in board size container
+ */
 BOARD_SIZE_BUTTONS.forEach((container) => {
   container.addEventListener("mouseenter", () => {
     handleMouseEnter(container, ACTIVELY_SELECTED_RADIOS.boardSize);
@@ -167,6 +265,12 @@ BOARD_SIZE_BUTTONS.forEach((container) => {
   });
 });
 
+/**
+ * handeling mouse enter event on a set of input buttons in setting page containers and activate their visuals
+ * @param container one of three html-div containers in setting page
+ * @param selectedElement
+ * @returns
+ */
 function handleMouseEnter(
   container: HTMLDivElement,
   selectedElement: HTMLInputElement | null,
@@ -182,6 +286,12 @@ function handleMouseEnter(
   activateVisualls(container);
 }
 
+/**
+ * handeling mouse leave event on a set of input buttons in setting page containers and accordingly reset their visuals
+ * @param container one of three html-div containers in setting page
+ * @param selectedElement
+ * @returns
+ */
 function handleMouseLeave(
   container: HTMLDivElement,
   selectedElement: HTMLInputElement | null,
@@ -203,18 +313,30 @@ function handleMouseLeave(
   }
 }
 
+/**
+ * handeling mouse over event on play button on intro page
+ */
 PLAY_BTN.addEventListener("mouseover", () => {
   PLAY_BUTTON_ARROW.src = ARROW_HOVER;
 });
 
+/**
+ * handeling mouse leave event on play button on intro page
+ */
 PLAY_BTN.addEventListener("mouseleave", () => {
   PLAY_BUTTON_ARROW.src = ARROW_NORMAL;
 });
 
+/**
+ * handeling click event on play button on intro page
+ */
 PLAY_BTN.addEventListener("click", () => {
   renderSettingsPage();
 });
 
+/**
+ * handeling click event on selected setting box to make necessary visual changes
+ */
 SETTING_BOX.addEventListener("click", () => {
   if (isSettingsComplete() && !SETTING_BOX.classList.contains("streched")) {
     updateSettingBoxTexts();
@@ -223,6 +345,9 @@ SETTING_BOX.addEventListener("click", () => {
   }
 });
 
+/**
+ * updating selected settings box text after clicking on it and show the selected options
+ */
 function updateSettingBoxTexts(): void {
   const THEME = document.getElementById("game-theme-text") as HTMLSpanElement;
   const PLAYER = document.getElementById("player-text") as HTMLSpanElement;
@@ -233,6 +358,9 @@ function updateSettingBoxTexts(): void {
   SIZE.innerText = `${gameLogic.currentRows}x${gameLogic.currentColumns}-Cards`;
 }
 
+/**
+ * updating selected settings box visuals after clicking on it
+ */
 function updateSettingBoxVisuals(): void {
   const YELLOW_LINES = SETTING_BOX.querySelectorAll(
     ".selected-setting img",
@@ -242,14 +370,23 @@ function updateSettingBoxVisuals(): void {
   });
 }
 
+/**
+ * handeling click event on start button to lead to board page
+ */
 START_BTN.addEventListener("click", () => {
   renderBoardElements(gameLogic.currentTheme);
 });
 
+/**
+ * handeling mouse over event on exit button to make visual changes
+ */
 EXIT_BTN?.addEventListener("mouseover", () => {
   if (EXIT_BTN_ICON) EXIT_BTN_ICON.src = EXIT_HOVER;
 });
 
+/**
+ * handeling mouse leave event on exit button to make visual changes
+ */
 EXIT_BTN?.addEventListener("mouseleave", () => {
   if (EXIT_BTN_ICON && gameLogic.currentTheme === "DAProjects")
     EXIT_BTN_ICON.src = EXIT_NORMAL_BLUE;
@@ -257,4 +394,7 @@ EXIT_BTN?.addEventListener("mouseleave", () => {
     EXIT_BTN_ICON.src = EXIT_NORMAL_ORANGE;
 });
 
+/**
+ * handeling click event on exit button to open the dialog.
+ */
 EXIT_BTN?.addEventListener("click", openDialog);
